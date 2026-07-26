c387a340e26311b324f523f3db57b77be377a005
hyunsooko <109939886+hyunsooko@users.noreply.github.com>
2026-07-26 14:18:06 +0900

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,9 +1,1 @@
-const slider=document.querySelector("#temperatureSlider");
-
-const star=document.querySelector(".star");
-
-const starName=document.querySelector("#starName");
-
-const starType=document.querySelector("#starType");
-
-const temperature=document.querySelector("#temperature");
+// dom.js
